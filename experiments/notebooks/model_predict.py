ee4a0c5618387e01536c54aae009654f5581b035
David Abraham <daveynoire97@gmail.com>
2019-09-22 22:34:35 +0530

    fixed access to uploaded image.updating webpage without reloading.

--- experiments/notebooks/model_predict.py
+++ experiments/notebooks/model_predict.py
@@ -22,7 +22,7 @@ def upload_page():
 @app.route('/predict',methods = ['GET','POST'])
 def predict():
     image_labels = ['A','B','C']
-    image_path = "/Users/davidabraham/gesture-lingua-backend/experiments/notebooks/"
+    image_path = join('uploaded_images','image.jpg')
     # create a file handler
     handler = logging.FileHandler('Log_Model_Builder.log')
     handler.setLevel(logging.DEBUG)
@@ -30,27 +30,20 @@ def predict():
     # create a logging format
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
-    #
-    # # add the handlers to the logger
+
+    # add the handlers to the logger
     logger.addHandler(handler)
 
-    #content = request.get_json()
-    #image_name = content.get("image_name")
-    #filename = join(image_path,image_name)
-
     file = request.files['file']
-    file.save(join('uploaded_images','image.jpg'))
+    file.save(image_path)
     model = load_model('model.h5')
-    X = path_to_tensor(join('uploaded_images','image.jpg'),50)
+    X = path_to_tensor(image_path,50)
 
     y_probs = model.predict(X)
     y_preds = np.argmax(y_probs,axis = 1)
 
-    #return image_labels[y_preds[0]]
-    #return jsonify({"Alphabet" : image_labels[y_preds[0]]})
-    #logger.info("Alphabet : {}".format(image_labels[y_preds[0]]))
-    #return 'file uploaded successfully {}'.format(file.filename)
-    return render_template("success.html",alphabet = image_labels[y_preds[0]])
+    return jsonify({'alphabet' : image_labels[y_preds[0]]})
+    #return render_template("linguahome.html",alphabet = image_labels[y_preds[0]])
     #return render_template("success.html",alphabet = file)
 
 app.run(port = 5000)
