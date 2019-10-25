1e00135854917931a8c2cc8620165426a8d01d9f
David Abraham <daveynoire97@gmail.com>
2019-10-25 13:56:15 +0530

    added snapshot button to new html template. new route created for video.

--- experiments/notebooks/model_predict.py
+++ experiments/notebooks/model_predict.py
@@ -1,34 +1,37 @@
+from flask import Flask, jsonify, request, render_template
+from os.path import join
+import logging
+from datasets.sign_language import path_to_tensor
+from keras.models import load_model
+import tensorflow as tf
 import numpy as np
 np.random.seed(5)
-import tensorflow as tf
 tf.set_random_seed(2)
-from keras.models import load_model
-from datasets.sign_language import path_to_tensor
-import logging
 #import random
-from os.path import join
-from flask import Flask, jsonify, request, render_template
 app = Flask(__name__)
 
-### Enabling logger
+# Enabling logger
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-@app.route("/", methods = ["GET"])
+
+@app.route("/", methods=["GET"])
 def upload_page():
     return render_template("linguahome.html")
 
-@app.route('/predict',methods = ['GET','POST'])
+
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if request.form["submit_button"] == "Predict the Alphabet":
-        image_labels = ['A','B','C']
-        image_path = join('uploaded_images','image.jpg')
+        image_labels = ['A', 'B', 'C']
+        image_path = join('uploaded_images', 'image.jpg')
         # create a file handler
         handler = logging.FileHandler('Log_Model_Builder.log')
         handler.setLevel(logging.DEBUG)
 
         # create a logging format
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         handler.setFormatter(formatter)
 
         # add the handlers to the logger
@@ -37,16 +40,17 @@ def predict():
         file = request.files['file']
         file.save(image_path)
         model = load_model('model.h5')
-        X = path_to_tensor(image_path,50)
+        X = path_to_tensor(image_path, 50)
 
         y_probs = model.predict(X)
-        y_preds = np.argmax(y_probs,axis = 1)
+        y_preds = np.argmax(y_probs, axis=1)
 
         # return jsonify({'alphabet' : image_labels[y_preds[0]]})
-        return render_template("linguahome.html",name = image_labels[y_preds[0]])
+        return render_template("linguahome.html", name=image_labels[y_preds[0]])
         # return render_template("success.html",alphabet = file)
 
     elif request.form["submit_button"] == "Capture Video":
         return render_template("success.html")
 
-app.run(port = 5000)
+
+app.run(port=5000)
--- experiments/notebooks/model_predict2.py
+++ experiments/notebooks/model_predict2.py
@@ -1,57 +1,74 @@
-from fastai.vision import ImageDataBunch, get_transforms, cnn_learner, normalize, imagenet_stats, open_image, models
+from flask import Flask, jsonify, request, render_template
+from os.path import join
+import logging
+from pathlib import Path
+import pickle
+import torch
+import cv2
+from binascii import a2b_base64
+from io import BytesIO
+#from fastai.vision import ImageDataBunch, get_transforms, cnn_learner, normalize, imagenet_stats, open_image, models
+from fastai.vision import *
 import numpy as np
 np.random.seed(5)
-import torch
-import pickle
-from pathlib import Path
-import logging
-from os.path import join
-from flask import Flask, jsonify, request, render_template
 app = Flask(__name__)
 
-### Enabling logger
+# Enabling logger
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-@app.route("/", methods = ["GET"])
+path = Path("/Users/davidabraham/gesture-lingua-backend/experiments/notebooks")
+
+with open(path / "class_names.pkl", "rb") as pkl_file:
+    classes = pickle.load(pkl_file)
+
+data2 = ImageDataBunch.single_from_classes(
+    path, classes, ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
+learn = cnn_learner(data2, models.resnet34)
+learn.load('model-after-unfreeze')
+
+
+@app.route("/", methods=["GET"])
 def upload_page():
     return render_template("linguahome.html")
 
-@app.route('/predict',methods = ['GET','POST'])
+
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if request.form["submit_button"] == "Predict the Alphabet":
 
-        path = Path("/Users/davidabraham/gesture-lingua-backend/experiments/notebooks")
-
-        with open(path/"class_names.pkl","rb") as pkl_file:
-            classes = pickle.load(pkl_file)
-
-        image_path = join('uploaded_images','image.jpg')
+        image_path = join('uploaded_images', 'image.jpg')
 
         # create a file handler
         handler = logging.FileHandler('Log_Model_Builder.log')
         handler.setLevel(logging.DEBUG)
 
         # create a logging format
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         handler.setFormatter(formatter)
 
         # add the handlers to the logger
         logger.addHandler(handler)
 
         file = request.files['file']
-        file.save(str(path/image_path))
+        file.save(str(path / image_path))
 
-        data2 = ImageDataBunch.single_from_classes(path,classes,ds_tfms= get_transforms(),size = 224).normalize(imagenet_stats)
-        learn = cnn_learner(data2,models.resnet34)
-        learn.load('model-after-unfreeze')
+        img = open_image(path / image_path)
+        label, index, pred = learn.predict(img)
 
-        img = open_image(path/image_path)
-        label,index, pred = learn.predict(img)
-
-        return render_template("linguavideo.html",name = label,image = image_path)
+        return render_template("linguahome.html", name=label)
 
     elif request.form["submit_button"] == "Capture Video":
-        return render_template("success.html")
+        return render_template("linguavideo2.html")
+        # # return render_template("success.html")
 
-app.run(port = 5000)
+    @app.route('/video', methods=["GET", "POST"])
+    def face():
+        binary_data = a2b_base64(request.files['imgBase64'])
+        img = open_image(BytesIO(binary_data))
+        label, index, pred = learn.predict(img)
+        # analysis = { "predictions": label}
+        logger.info(label)
+
+app.run(port=5000)
